eeb7dd5481caf42d359ab88415fd41125627e8ce
gustavocanhan <gustavocanhan@gmail.com>
2026-03-01 17:33:08 -0300

    feat: add card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import ContentHome from "@/components/content/ContentHome";
+import CardContent from "@/components/card/CardContent";
 
 export default function Home() {
-  return <ContentHome />;
+  return (
+    <main className="max-w-7xl mx-auto p-8">
+      <h1>Olá, seja bem-vindo(a)!</h1>
+      <p>Veja abaixo o resumo das suas finanças</p>
+      <CardContent />
+    </main>
+  );
 }
